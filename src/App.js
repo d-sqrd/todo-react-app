bea0a098969567b53dfd04bf0e908d43f1a4cfd9
d-sqrd <deb.bunny98@gmail.com>
2022-04-21 21:52:34 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import Todo from './components/Todo';
+
+let todoNum = 0;
 
 function App() {
+  const [todoList, setTodoList] = useState([]);   // array that will store already created todos
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleChange = (e) => {
+    setNewTodo(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.newTodo.value);
+    let todo = {};
+    todo.id = todoNum++;
+    todo.desc = newTodo;
+    let updatedTodoList = [...todoList, todo];
+    setTodoList(updatedTodoList);
+  }
+
+  const deleteTodo = (todoId) => {
+    // console.log(todoId);
+    let updatedTodoList = todoList.filter(todo => todo.id !== todoId);
+    setTodoList(updatedTodoList);
+  }
+  
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input  type="text" 
+                name="newTodo"
+                value={newTodo}
+                onChange={handleChange}>
+        </input>
+        <input  type="submit" 
+                value="New Task">
+        </input>
+      </form>
+      <div id="todos-container">
+        {todoList.map((todo, index) => {
+          // console.log(index);
+          return(
+            <div key={index}>
+              <Todo   todo={todo} 
+                      deleteTodo={deleteTodo}/>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+
+function Todo({todo, deleteTodo}) {
+    const [todoText, setTodoText] = useState(todo.desc);
+    const [edit, setEdit] = useState(false);
+    const [editInput, setEditInput] = useState('');
+
+    const handleEdit = (e) => {
+        setEditInput(e.target.value);
+    }
+
+    const handleEditSubmit = (e) => {
+        e.preventDefault();
+        let edittedTodo = e.target.edittedTodoInput.value;
+        console.log(edittedTodo);
+        setTodoText(edittedTodo);
+        setEdit(!edit);
+        setEditInput('');
+    }
+
+    return(
+      <div>
+        <h1>{todoText}</h1>
+        <button onClick={() => setEdit(!edit)}>Update</button>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        {
+            edit ? 
+                <div>
+                  <form onSubmit={handleEditSubmit}>
+                    <input  type="text"
+                            name="edittedTodoInput" 
+                            value={editInput} 
+                            onChange={handleEdit}>
+                    </input>
+                    <input  type="submit" 
+                            value="Update Todo">
+                    </input>
+                  </form>
+                </div> :
+                ""
+        }
+      </div>
+    );
+  }
+
+
+  export default Todo;
